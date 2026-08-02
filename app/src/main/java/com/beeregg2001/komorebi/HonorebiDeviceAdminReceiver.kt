68c7b62b113d9a5294347b617474b3406bb9337d
huggy <i@huggy.moe>
2026-08-03 00:22:23 +0900

    refactor: move device policy tweaks to TVTweaks

--- app/src/main/java/com/beeregg2001/komorebi/HonorebiDeviceAdminReceiver.kt
+++
@@ -1,6 +0,0 @@
-package com.beeregg2001.komorebi
-
-import android.app.admin.DeviceAdminReceiver
-
-/** Device-owner entry point used for persistent TV policy management. */
-class HonorebiDeviceAdminReceiver : DeviceAdminReceiver()
--- app/src/main/java/com/beeregg2001/komorebi/MainActivity.kt
+++ app/src/main/java/com/beeregg2001/komorebi/MainActivity.kt
@@ -38,9 +38,6 @@ class MainActivity : ComponentActivity() {
         setTheme(R.style.Theme_Komorebi)
         super.onCreate(savedInstanceState)
 
-        AirPlayStartupRepairManager.repair(this)
-        handleDevicePolicyIntent(intent)
-
         // OS互換性のチェック (Android 8.0 API 26 以上が必要)
         val isOsCompatible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
@@ -78,26 +75,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        AirPlayStartupRepairManager.repair(this)
-        handleDevicePolicyIntent(intent)
         if (intent?.hasCategory(Intent.CATEGORY_HOME) == true) {
             homeIntentVersion++
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        DevicePolicyHomeManager.applyIfAuthorized(this)
-        LauncherComponentPolicyManager.disableHomeActivitiesIfAuthorized(this)
-    }
-
-    private fun handleDevicePolicyIntent(intent: Intent?) {
-        if (intent?.action == DevicePolicyHomeManager.ACTION_CLEAR_HOME_POLICY) {
-            DevicePolicyHomeManager.clearIfAuthorized(this)
-            LauncherComponentPolicyManager.restoreHomeActivitiesIfAuthorized(this)
-        } else {
-            DevicePolicyHomeManager.applyIfAuthorized(this)
-            LauncherComponentPolicyManager.disableHomeActivitiesIfAuthorized(this)
         }
     }
 }
